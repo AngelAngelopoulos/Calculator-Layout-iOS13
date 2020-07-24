b35d84d1e5a8e305a64c06acf0216c523fffaf35
AngelAngelopoulos <angelsuminoe01@gmail.com>
2020-07-23 20:20:03 -0500

    App Finished

--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -9,12 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textBox: UILabel!
+    
+    var operation :String = "";
+    var number1 :Int = 0;
+    var number2 :Int = 0;
+    var result :Int = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    
+    
+    @IBAction func buttonDivPressed(_ sender: Any) {
+        
+    }
+    
+    
+    /*@IBAction func buttonEqualPressed(_ sender: Any) {
+        switch operation {
+        
+        case "+":
+            textBox.text = String.init((number1 + number2));
+            operation = "";
+            break;
+            
+        case "-":
+        textBox.text = String.init((number1 - number2));
+        operation = "";
+        break;
+        
+        case "/":
+        textBox.text = String.init((number1 / number2));
+        operation = "";
+        break;
+        
+        case "*":
+        textBox.text = String.init((number1 * number2));
+        operation = "";
+        break;
+            
+        default:
+            textBox.text = String.init(number1);
+        }
+    }*/
+    
 }
 
